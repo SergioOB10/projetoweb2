79ac2dbe191fba6d902aec64a9a2022b1bfbdb1c
Sérgio Oliveira <152030640+SergioOB10@users.noreply.github.com>
2024-04-24 21:47:36 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    //cadastra o jogado
+    document.getElementById("btnCadastrar").addEventListener("click", function() {
+        var jogador = {
+            nome: document.getElementById("nome").value,
+            time: document.getElementById("time").value,
+            gols: document.getElementById("gols").value,
+            assistencias: document.getElementById("assistencias").value,
+            foto: document.getElementById("foto").value
+        };
+    
+        var jogadores = JSON.parse(localStorage.getItem('jogadores')) || [];
+    
+        jogadores.push(jogador);
+    
+        localStorage.setItem('jogadores', JSON.stringify(jogadores));
+    
+        window.location.href = "index.html";
+    });
+
+    //exclui o jogador
+    var jogadoresListContainer = document.getElementById('jogadores-list');
+    jogadoresListContainer.addEventListener('click', function(event) {
+        if (event.target.classList.contains('delete-player')) {
+            var jogadorIndex = event.target.dataset.index;
+            var jogadores = JSON.parse(localStorage.getItem('jogadores')) || [];
+            jogadores.splice(jogadorIndex, 1);
+            localStorage.setItem('jogadores', JSON.stringify(jogadores));
+
+            //atualiza a pagina
+            location.reload(); 
+        }
+    });
+});
